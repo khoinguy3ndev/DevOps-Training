2a91ebcacedcb7c0d3bce33477af4f7c8e3fa0f9
Nguyen Khoi <khoibene@gmail.com>
2026-06-19 00:16:47 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT ?? 3000;
 
 console.log('Hello World')
